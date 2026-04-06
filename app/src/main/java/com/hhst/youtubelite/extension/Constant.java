f71f251381d39511d0a213d615acfc5f6a204b1a
Sahil Kumarv <sahilisthebest885@gmail.com>
2026-04-06 12:15:23 +0500

    refactor: update extension system base and constants

--- app/src/main/java/com/hhst/youtubelite/extension/Constant.java
+++ app/src/main/java/com/hhst/youtubelite/extension/Constant.java
@@ -1,6 +1,7 @@
 package com.hhst.youtubelite.extension;
 
 import static com.hhst.youtubelite.Constant.ENABLE_BACKGROUND_PLAY;
+import static com.hhst.youtubelite.Constant.ENABLE_IN_APP_MINI_PLAYER;
 import static com.hhst.youtubelite.Constant.ENABLE_PIP;
 import static com.hhst.youtubelite.Constant.REMEMBER_LAST_POSITION;
 import static com.hhst.youtubelite.Constant.REMEMBER_RESIZE_MODE;
@@ -8,27 +9,93 @@ import static com.hhst.youtubelite.Constant.SKIP_POI_HIGHLIGHT;
 import static com.hhst.youtubelite.Constant.SKIP_SELF_PROMO;
 import static com.hhst.youtubelite.Constant.SKIP_SPONSORS;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Constant {
 	public static final String ENABLE_DISPLAY_DISLIKES = "enable_display_dislikes";
-	public static final String ENABLE_HIDE_SHORTS = "enable_hide_shorts";
-	public static final String REMEMBER_QUALITY = "remember_quality";
-	public static final String REMEMBER_PLAYBACK_SPEED = "remember_playback_speed";
+	public static final String DEFAULT_QUALITY = "default_quality";
+	public static final String DEFAULT_PLAYBACK_SPEED = "default_playback_speed";
 	public static final String ENABLE_PLAYER_GESTURES = "enable_player_gestures";
+	
+	public static final String NAV_BAR_ORDER = "nav_bar_order";
+	public static final String NAV_BAR_SHOW_HOME = "nav_bar_show_home";
+	public static final String NAV_BAR_SHOW_SHORTS = "nav_bar_show_shorts";
+	public static final String NAV_BAR_SHOW_SUBSCRIPTIONS = "nav_bar_show_subscriptions";
+	public static final String NAV_BAR_SHOW_LIBRARY = "nav_bar_show_library";
+	public static final String NAV_BAR_SHOW_DOWNLOADS = "nav_bar_show_downloads";
+	public static final String NAV_BAR_SHOW_SETTINGS = "nav_bar_show_settings";
+	public static final String SHOW_NAV_BAR_IN_SHORTS = "show_nav_bar_in_shorts";
+	public static final String HIDE_NAV_BAR_LABELS = "hide_nav_bar_labels";
 
-	public static final Map<String, Boolean> DEFAULT_PREFERENCES = Map.ofEntries(
-					Map.entry(ENABLE_DISPLAY_DISLIKES, true),
-					Map.entry(ENABLE_HIDE_SHORTS, false),
-					Map.entry(SKIP_SPONSORS, true),
-					Map.entry(SKIP_SELF_PROMO, true),
-					Map.entry(SKIP_POI_HIGHLIGHT, true),
-					Map.entry(REMEMBER_LAST_POSITION, true),
-					Map.entry(REMEMBER_QUALITY, true),
-					Map.entry(ENABLE_BACKGROUND_PLAY, true),
-					Map.entry(ENABLE_PIP, true),
-					Map.entry(REMEMBER_RESIZE_MODE, false),
-					Map.entry(REMEMBER_PLAYBACK_SPEED, false),
-					Map.entry(ENABLE_PLAYER_GESTURES, true)
-	);
+	public static final String DEFAULT_NAV_BAR_ORDER = "home,shorts,subscriptions,library,downloads,settings";
+	public static final String ACTION_BAR_ORDER = "action_bar_order";
+	public static final String ACTION_BAR_SHOW_SHARE = "action_bar_show_share";
+	public static final String ACTION_BAR_SHOW_REMIX = "action_bar_show_remix";
+	public static final String ACTION_BAR_SHOW_DOWNLOAD = "action_bar_show_download";
+	public static final String ACTION_BAR_SHOW_THANKS = "action_bar_show_thanks";
+	public static final String ACTION_BAR_SHOW_CLIP = "action_bar_show_clip";
+	public static final String ACTION_BAR_SHOW_SAVE = "action_bar_show_save";
+	public static final String ACTION_BAR_SHOW_REPORT = "action_bar_show_report";
+	public static final String ACTION_BAR_SHOW_ASK_AI = "action_bar_show_ask_ai";
+
+	public static final String DEFAULT_ACTION_BAR_ORDER = "like_dislike,download,pip,share,remix,thanks,clip,save,report,ask_ai";
+
+	public static final String HIDE_SHORTS = "hide_shorts";
+	public static final String HIDE_COMMENTS = "hide_comments";
+	public static final String HIDE_RECOMMENDATIONS = "hide_recommendations";
+	public static final String CUSTOMIZE_PLAYER_BUTTONS = "player_internal_buttons";
+	public static final String PLAYER_SHOW_SPEED = "player_show_speed";
+	public static final String PLAYER_SHOW_QUALITY = "player_show_quality";
+	public static final String PLAYER_SHOW_SUBTITLES = "player_show_subtitles";
+	public static final String PLAYER_SHOW_SEGMENTS = "player_show_segments";
+	public static final String PLAYER_SHOW_LOOP = "player_show_loop";
+	public static final String PLAYER_SHOW_RELOAD = "player_show_reload";
+	public static final String PLAYER_SHOW_LOCK = "player_show_lock";
+	public static final String PLAYER_SHOW_NEXT = "player_show_next";
+	public static final String PLAYER_SHOW_PREVIOUS = "player_show_previous";
+	public static final String DOWNLOAD_LOCATION = "download_location";
+
+	public static final Map<String, Boolean> DEFAULT_PREFERENCES = new HashMap<>();
+	static {
+		DEFAULT_PREFERENCES.put(ENABLE_DISPLAY_DISLIKES, true);
+		DEFAULT_PREFERENCES.put(SKIP_SPONSORS, true);
+		DEFAULT_PREFERENCES.put(SKIP_SELF_PROMO, true);
+		DEFAULT_PREFERENCES.put(SKIP_POI_HIGHLIGHT, true);
+		DEFAULT_PREFERENCES.put(REMEMBER_LAST_POSITION, true);
+		DEFAULT_PREFERENCES.put(ENABLE_BACKGROUND_PLAY, true);
+		DEFAULT_PREFERENCES.put(ENABLE_PIP, true);
+		DEFAULT_PREFERENCES.put(ENABLE_IN_APP_MINI_PLAYER, true);
+		DEFAULT_PREFERENCES.put(REMEMBER_RESIZE_MODE, false);
+		DEFAULT_PREFERENCES.put(ENABLE_PLAYER_GESTURES, true);
+		DEFAULT_PREFERENCES.put(NAV_BAR_SHOW_HOME, true);
+		DEFAULT_PREFERENCES.put(NAV_BAR_SHOW_SHORTS, true);
+		DEFAULT_PREFERENCES.put(NAV_BAR_SHOW_SUBSCRIPTIONS, true);
+		DEFAULT_PREFERENCES.put(NAV_BAR_SHOW_LIBRARY, true);
+		DEFAULT_PREFERENCES.put(NAV_BAR_SHOW_DOWNLOADS, true);
+		DEFAULT_PREFERENCES.put(NAV_BAR_SHOW_SETTINGS, true);
+		DEFAULT_PREFERENCES.put(SHOW_NAV_BAR_IN_SHORTS, true);
+		DEFAULT_PREFERENCES.put(HIDE_NAV_BAR_LABELS, false);
+		DEFAULT_PREFERENCES.put(ACTION_BAR_SHOW_SHARE, true);
+		DEFAULT_PREFERENCES.put(ACTION_BAR_SHOW_REMIX, true);
+		DEFAULT_PREFERENCES.put(ACTION_BAR_SHOW_DOWNLOAD, true);
+		DEFAULT_PREFERENCES.put(ACTION_BAR_SHOW_THANKS, true);
+		DEFAULT_PREFERENCES.put(ACTION_BAR_SHOW_CLIP, true);
+		DEFAULT_PREFERENCES.put(ACTION_BAR_SHOW_SAVE, true);
+		DEFAULT_PREFERENCES.put(ACTION_BAR_SHOW_REPORT, true);
+		DEFAULT_PREFERENCES.put(ACTION_BAR_SHOW_ASK_AI, true);
+
+		DEFAULT_PREFERENCES.put(HIDE_SHORTS, false);
+		DEFAULT_PREFERENCES.put(HIDE_COMMENTS, false);
+		DEFAULT_PREFERENCES.put(HIDE_RECOMMENDATIONS, false);
+		DEFAULT_PREFERENCES.put(PLAYER_SHOW_SPEED, true);
+		DEFAULT_PREFERENCES.put(PLAYER_SHOW_QUALITY, true);
+		DEFAULT_PREFERENCES.put(PLAYER_SHOW_SUBTITLES, true);
+		DEFAULT_PREFERENCES.put(PLAYER_SHOW_SEGMENTS, true);
+		DEFAULT_PREFERENCES.put(PLAYER_SHOW_LOOP, true);
+		DEFAULT_PREFERENCES.put(PLAYER_SHOW_RELOAD, true);
+		DEFAULT_PREFERENCES.put(PLAYER_SHOW_LOCK, true);
+		DEFAULT_PREFERENCES.put(PLAYER_SHOW_NEXT, true);
+		DEFAULT_PREFERENCES.put(PLAYER_SHOW_PREVIOUS, true);
+	}
 }
--- app/src/main/java/com/hhst/youtubelite/extension/Extension.java
+++ app/src/main/java/com/hhst/youtubelite/extension/Extension.java
@@ -1,42 +1,75 @@
 package com.hhst.youtubelite.extension;
 
-import static com.hhst.youtubelite.Constant.ENABLE_BACKGROUND_PLAY;
-import static com.hhst.youtubelite.Constant.ENABLE_PIP;
-import static com.hhst.youtubelite.Constant.REMEMBER_LAST_POSITION;
-import static com.hhst.youtubelite.Constant.REMEMBER_RESIZE_MODE;
-import static com.hhst.youtubelite.Constant.SKIP_POI_HIGHLIGHT;
-import static com.hhst.youtubelite.Constant.SKIP_SELF_PROMO;
-import static com.hhst.youtubelite.Constant.SKIP_SPONSORS;
-
 import com.hhst.youtubelite.R;
-
-import java.util.Collections;
 import java.util.List;
 
-public record Extension(String key, int description, List<Extension> children) {
+import static com.hhst.youtubelite.Constant.*;
 
-	public static List<Extension> defaultExtensionTree() {
-		return List.of(
-						new Extension(null, R.string.general, List.of(
-										new Extension(Constant.ENABLE_DISPLAY_DISLIKES, R.string.display_dislikes, null),
-										new Extension(Constant.ENABLE_HIDE_SHORTS, R.string.hide_shorts, null),
-										new Extension(ENABLE_PIP, R.string.pip, null)
-						)),
-						new Extension(null, R.string.player, List.of(
-										new Extension(REMEMBER_LAST_POSITION, R.string.remember_last_position, null),
-										new Extension(Constant.REMEMBER_QUALITY, R.string.remember_quality, null),
-										new Extension(Constant.REMEMBER_PLAYBACK_SPEED, R.string.remember_playback_speed, null),
-										new Extension(REMEMBER_RESIZE_MODE, R.string.remember_resize_mode, null),
-										new Extension(Constant.ENABLE_PLAYER_GESTURES, R.string.player_gestures, null),
-										new Extension(ENABLE_BACKGROUND_PLAY, R.string.background_play, null)
-						)),
-						new Extension(null, R.string.skip_sponsors, List.of(
-										new Extension(SKIP_SPONSORS, R.string.skip_sponsors, null),
-										new Extension(SKIP_SELF_PROMO, R.string.skip_sponsors_selfpromo, null),
-										new Extension(SKIP_POI_HIGHLIGHT, R.string.skip_sponsors_highlight, null)
-						)),
-						new Extension(null, R.string.download, Collections.emptyList()),
-						new Extension(null, R.string.miscellaneous, Collections.emptyList())
-		);
-	}
+public record Extension(String key, int description, int helpText, List<Extension> children) {
+
+    public Extension(String key, int description, List<Extension> children) {
+        this(key, description, 0, children);
+    }
+
+    public static List<Extension> defaultExtensionTree() {
+        return List.of(
+            new Extension(null, R.string.general, List.of(
+                new Extension(Constant.NAV_BAR_ORDER, R.string.custom_navigation, List.of(
+                    new Extension(Constant.NAV_BAR_SHOW_HOME, R.string.nav_home, null),
+                    new Extension(Constant.NAV_BAR_SHOW_SHORTS, R.string.nav_shorts, null),
+                    new Extension(Constant.NAV_BAR_SHOW_SUBSCRIPTIONS, R.string.nav_subscriptions, null),
+                    new Extension(Constant.NAV_BAR_SHOW_LIBRARY, R.string.nav_library, null),
+                    new Extension(Constant.NAV_BAR_SHOW_DOWNLOADS, R.string.nav_downloads, null),
+                    new Extension(Constant.NAV_BAR_SHOW_SETTINGS, R.string.nav_settings, null)
+                )),
+                new Extension(Constant.SHOW_NAV_BAR_IN_SHORTS, R.string.show_nav_bar_in_shorts, null),
+                new Extension(Constant.HIDE_NAV_BAR_LABELS, R.string.hide_nav_bar_labels,  null),
+                new Extension(Constant.HIDE_SHORTS, R.string.hide_shorts, R.string.hide_shorts_desc, null),
+                new Extension(Constant.ENABLE_DISPLAY_DISLIKES, R.string.return_dislike, null)
+            )),
+            new Extension(null, R.string.player, List.of(
+                new Extension(Constant.ACTION_BAR_ORDER, R.string.custom_action_bar, R.string.custom_action_bar_desc, List.of(
+                    new Extension(Constant.ACTION_BAR_SHOW_DOWNLOAD, R.string.action_download, null),
+                    new Extension(ENABLE_PIP, R.string.pip, null),
+                    new Extension(Constant.ACTION_BAR_SHOW_SHARE, R.string.action_share, null),
+                    new Extension(Constant.ACTION_BAR_SHOW_REMIX, R.string.action_remix, null),
+                    new Extension(Constant.ACTION_BAR_SHOW_THANKS, R.string.action_thanks, null),
+                    new Extension(Constant.ACTION_BAR_SHOW_CLIP, R.string.action_clip, null),
+                    new Extension(Constant.ACTION_BAR_SHOW_SAVE, R.string.action_save, null),
+                    new Extension(Constant.ACTION_BAR_SHOW_REPORT, R.string.action_report, null),
+                    new Extension(Constant.ACTION_BAR_SHOW_ASK_AI, R.string.action_ask_ai, null)
+                )),
+                new Extension(Constant.CUSTOMIZE_PLAYER_BUTTONS, R.string.player_buttons, R.string.player_buttons_desc, List.of(
+                    new Extension(Constant.PLAYER_SHOW_SPEED, R.string.speed, null),
+                    new Extension(Constant.PLAYER_SHOW_QUALITY, R.string.quality, null),
+                    new Extension(Constant.PLAYER_SHOW_SUBTITLES, R.string.subtitles, null),
+                    new Extension(Constant.PLAYER_SHOW_SEGMENTS, R.string.segments, null),
+                    new Extension(Constant.PLAYER_SHOW_LOOP, R.string.loop, null),
+                    new Extension(Constant.PLAYER_SHOW_RELOAD, R.string.restart, null),
+                    new Extension(Constant.PLAYER_SHOW_LOCK, R.string.lock_screen, null),
+                    new Extension(Constant.PLAYER_SHOW_NEXT, R.string.action_next, null),
+                    new Extension(Constant.PLAYER_SHOW_PREVIOUS, R.string.action_previous, null)
+                )),
+                new Extension(REMEMBER_LAST_POSITION, R.string.remember_last_position, null),
+                new Extension(Constant.DEFAULT_QUALITY, R.string.default_quality, null),
+                new Extension(Constant.DEFAULT_PLAYBACK_SPEED, R.string.default_playback_speed, null),
+                new Extension(REMEMBER_RESIZE_MODE, R.string.remember_resize_mode, null),
+                new Extension(Constant.ENABLE_PLAYER_GESTURES, R.string.player_gestures, R.string.player_gestures_desc, null),
+                new Extension(ENABLE_IN_APP_MINI_PLAYER, R.string.in_app_mini_player, R.string.in_app_mini_player_desc, null),
+                new Extension(ENABLE_BACKGROUND_PLAY, R.string.background_play, R.string.background_play_desc, null)
+            )),
+            new Extension(null, R.string.skip_sponsors, List.of(
+                new Extension(SKIP_SPONSORS, R.string.skip_sponsors, null),
+                new Extension(SKIP_SELF_PROMO, R.string.skip_sponsors_selfpromo, null),
+                new Extension(SKIP_POI_HIGHLIGHT, R.string.skip_sponsors_highlight, null)
+            )),
+            new Extension(null, R.string.download, List.of(
+                new Extension(Constant.DOWNLOAD_LOCATION, R.string.download_location, null)
+            )),
+            new Extension(null, R.string.miscellaneous, List.of(
+                new Extension(Constant.HIDE_COMMENTS, R.string.hide_comments, null),
+                new Extension(Constant.HIDE_RECOMMENDATIONS, R.string.hide_recommendations, null)
+            ))
+        );
+    }
 }
