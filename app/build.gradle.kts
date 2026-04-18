1835c1217149daf85ee89608429696d391ed9b25
halcyra <hhstyyds520@gmail.com>
2026-04-18 21:48:51 +0800

    Update to v2.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.hhst.litube"
         minSdk = 26
         targetSdk = 36
-        versionCode = 212
-        versionName = "v2.1.2"
+        versionCode = 213
+        versionName = "v2.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
